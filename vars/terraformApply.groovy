3a5fb55b6e2ba972fe8d002576039fdca5261767
Rory Chatterton <rorychatt@gmail.com>
2017-11-04 23:40:32 +1100

    Syntax Fix

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -16,7 +16,7 @@ def call(body) {
             // Run Apply
             sh 'pip3 install -r terraform_enterprise_2/run_jobs/requirements.txt'
             
-            sh "set +e; python3 terraform_enterprise_2/run_jobs/run_terraform_apply.py \'${config.org}\' \'${config.app_id}\' \'${config.workspace}\' \'${plan_info.props['run_id']}\' \'${config.destroy_flag}\' \'${config.tf_token}\'"
+            sh "set +e; python3 terraform_enterprise_2/run_jobs/run_terraform_apply.py \'${config.org}\' \'${config.app_id}\' \'${config.workspace}\' \'${plan_info.['run_id']}\' \'${config.destroy_flag}\' \'${config.tf_token}\'"
 
             results = readJSON file: 'data.json'
 
@@ -24,7 +24,7 @@ def call(body) {
                 currentBuild.result = 'SUCCESS'
                 sh 'echo "Successfully Applied!"'
                 results.changes = False
-            } else if results['status'] == "errored") {
+            } (else if results['status'] == "errored") {
                 currentBuild.result = 'FAILURE'
                 sh 'echo "Apply Failed!"'
                 results.changes = False
