5ccb712fb38463699f5a6aa37f0d171678f44e59
Rory Chatterton <rorychatt@gmail.com>
2017-11-04 21:43:12 +1100

    explicitely declared vars

--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -1,11 +1,7 @@
+def call(string testoutput) {
 
-def call(body) {
-
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
     stage name: 'Stage', concurrency: 1 {
+        print "yo"
         print "Output: ${config.testoutput}"
     }
 }
