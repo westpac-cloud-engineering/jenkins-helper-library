d25fc22e5b07dabeb2cb7be5b83ccfb61883bde2
Rory Chatterton <rorychatt@gmail.com>
2017-11-04 22:01:30 +1100

    explicitely declared vars

--- vars/testFunction.groovy
+++ vars/testFunction.groovy
@@ -1,8 +1,11 @@
 def call(body) {
+
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
+    print "This is a test of a global DSL"
 
     stage name: 'Stage', concurrency: 1 {
         print "This is a test of a global DSL"
