e8ed738f305ec59e6ee9700b122464625759f588
Rory Chatterton <rchatterton@westpac.com.au>
2017-11-14 09:58:44 +1100

    Paramters ordered wrong. TODO: Named Parameters

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -11,13 +11,14 @@ def call(body) {
 
             // Download Terraform Helper Scripts
             print 'echo "Getting Terraform Scripts"'
-            git url: 'https://github.com/Westpac/cloud_deployment_helpers'
-            writeJSON file: 'environment_variables.json', json: config.credentials
+            dir('helper_scripts') {
+                    git url: 'https://github.com/Westpac/cloud_deployment_helpers'
+            }
 
             // Run Apply
             sh 'pip3 install -r terraform_enterprise_2/run_jobs/requirements.txt'
             
-            sh "set +e; python3 terraform_enterprise_2/run_jobs/run_terraform_apply.py \'${config.app_id}\' \'${config.component_name}\' \'${config.environment}\' \'${config.destroy_flag}\' \'${config.plan_info.run_id}\' "
+            sh "set +e; python3 helper_scripts/terraform_enterprise_2/run_jobs/run_terraform_apply.py \'${config.app_id}\' \'${config.component_name}\' \'${config.environment}\' \'${config.plan_info.run_id}\' \'${config.destroy_flag}\'  "
 
             results = readJSON file: 'data.json'
 
