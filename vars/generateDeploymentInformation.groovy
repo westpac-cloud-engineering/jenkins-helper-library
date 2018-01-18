702d3013ea1b422b6e6bc2b535f03b31607314e4
Rory Chatterton <me@rorychatterton.com>
2018-01-18 23:13:32 +1100

    Nested Map Syntax issues

--- vars/generateDeploymentInformation.groovy
+++ vars/generateDeploymentInformation.groovy
@@ -43,7 +43,7 @@ def call(body) {
             return deployment_map
         }
         def jsonMap = readJSON text: JsonOutput.toJson(deployment_map)
-        print prettyPrint(jsonmap)
+        print prettyPrint(jsonMap)
         return jsonMap
     }
 }
